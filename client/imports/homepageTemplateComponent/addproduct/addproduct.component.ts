484e4640f222c6c34db2791f416e6179ce5b6d60
upexcel <excellencetechnologies@gmail.com>
2016-10-14 17:59:58 +0530

    issue #11 fixed

--- client/imports/homepageTemplateComponent/addproduct/addproduct.component.ts
+++ client/imports/homepageTemplateComponent/addproduct/addproduct.component.ts
@@ -4,12 +4,9 @@ import { Mongo } from 'meteor/mongo';
 import { Meteor } from 'meteor/meteor';
 import { MeteorComponent } from 'angular2-meteor';
 import { REACTIVE_FORM_DIRECTIVES, FormGroup, FormBuilder ,Validators} from '@angular/forms';
-
 import { Productcategory }   from '../../../../both/collections/csvdata.collection';
-
 import template from './addproduct.html';
  
-
 @Component({
   selector: 'csvaddproduct',
   template,
@@ -19,66 +16,55 @@ import template from './addproduct.html';
 export class CsvAddProductComponent extends MeteorComponent implements OnInit {
   productlist: Mongo.Cursor<any>;
   subcategory:Mongo.Cursor<any>;
-  
   addForm: FormGroup;
   addFormsubcategory: FormGroup;
-  
   selectedCategory: any;
   activateChild:boolean;
+  
   constructor(private formBuilder: FormBuilder,private _router:Router) {
       super();
   }
   
-  onSelect(category: any): void {
+onSelect(category: any): void {
   this.selectedCategory = category;
   this.activateChild=true;
   this.subcategory=category;
-//  Productcategory.find({_id:category._id},{"subarray":1,_id:0,"category":0});
-  console.log(this.subcategory);
 }
   
-  ngOnInit() {
+ngOnInit() {
       this.subscribe('Productcategory', () => {
       this.productlist = Productcategory.find();
     }, true);
-//    **** for checking user is login or not ****  
     if (!Meteor.userId()) {
         this._router.navigate(['/login']);
     }
-    this.productlist = Productcategory.find();
-    
-  
-    
-    console.log(this.productlist);
-    this.addForm = this.formBuilder.group({
+   this.productlist = Productcategory.find();
+
+   this.addForm = this.formBuilder.group({
       category: ['', Validators.required],
     });
     
-    this.addFormsubcategory = this.formBuilder.group({
+   this.addFormsubcategory = this.formBuilder.group({
       subcategory: ['', Validators.required],
     });
   }
-  resetForm() {
+resetForm() {
     this.addForm.controls['category']['updateValue']('');
   }
   
-  ressetChildForm(){
+ressetChildForm(){
       this.addFormsubcategory.controls['subcategory']['updateValue'](''); 
   }
   
- addcategory(){
+addcategory(){
      if(this.addForm.valid){
          Productcategory.insert(this.addForm.value);
-         
-         // to empty the input box
          this.resetForm();
      }
  }
  
- addSubcategory(parentCategory_id){
+addSubcategory(parentCategory_id){
      if(this.addFormsubcategory.valid){
-         console.log(parentCategory_id);
-         console.log(this.addFormsubcategory.controls['subcategory'].value);
          Productcategory.update(
              {_id:parentCategory_id},{ 
            $push:{ "subarray":
@@ -86,21 +72,14 @@ export class CsvAddProductComponent extends MeteorComponent implements OnInit {
                "subcategory":this.addFormsubcategory.controls['subcategory'].value
                }
            }
-             });
-//             Teams.update(teamId, {$set: {name: "Something else"}}); 
-//         Productcategory.insert(this.addFormsubcategory.value);// prototype
-//         { $push: { scores: 89 } }
-         
+             });      
          this.ressetChildForm();
      }
  }
  
  updatecategory(){
      if(this.addForm.valid){
-//         Productcategory.insert(this.addForm.value);
-           Productcategory.update({_id:this.selectedCategory._id},{$set:{"category":this.selectedCategory.category}});
-         
-         // to empty the input box
+         Productcategory.update({_id:this.selectedCategory._id},{$set:{"category":this.selectedCategory.category}});
          this.resetForm();
          this.selectedCategory="";
          this.activateChild="";
@@ -111,10 +90,7 @@ export class CsvAddProductComponent extends MeteorComponent implements OnInit {
      Productcategory.remove(category._id);
  }
  removeSubCategory(id,subarraycategoryname){
-     console.log(id);
-     console.log(subarraycategoryname);
      Productcategory.update({ _id : id },{ $pull : {'subarray' : { 'subcategory' : subarraycategoryname } } });
-//     this.activateChild=false;
  }
  
   
