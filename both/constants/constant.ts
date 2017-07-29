993032ae0b13dff7ab679a525a731473ffe6f277
ashish <ashish_singh@excellencetechnologies.in>
2017-07-29 14:25:27 +0530

    added sendgrid mailing domain

--- both/constants/constant.ts
+++ both/constants/constant.ts
@@ -1,3 +1,3 @@
 export const startingMonth = 4;
 export const endingMonth = 3;
-export const urlEmailing='smtp://postmaster%40sandbox2599d4676ee6496689a2aa20ba27ac82.mailgun.org:c894bd1a245a8f2bbdd7a1ca95721092@smtp.mailgun.org:587';
+export const urlEmailing='smtp://apikey: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@smtp.sendgrid.net:587';
