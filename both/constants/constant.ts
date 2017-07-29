f80266ffd4bb74b6895242507f4f164567686f02
ashish <ashish_singh@excellencetechnologies.in>
2017-07-29 14:44:50 +0530

    added config file

--- both/constants/constant.ts
+++ both/constants/constant.ts
@@ -1,3 +1,2 @@
 export const startingMonth = 4;
 export const endingMonth = 3;
-export const urlEmailing='smtp://postmaster%40sandbox2599d4676ee6496689a2aa20ba27ac82.mailgun.org:c894bd1a245a8f2bbdd7a1ca95721092@smtp.mailgun.org:587';
