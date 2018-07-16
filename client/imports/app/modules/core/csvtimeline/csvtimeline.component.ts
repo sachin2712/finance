427b67493276aa93b5d1307e3a30be7dc64a8fb6
upexcel <rahul.excel2011@gmail.com>
2018-07-16 11:01:15 +0530

    added check condition for thismonthopenbalance

--- client/imports/app/modules/core/csvtimeline/csvtimeline.component.ts
+++ client/imports/app/modules/core/csvtimeline/csvtimeline.component.ts
@@ -949,7 +949,7 @@ export class CsvTimelineComponent implements OnInit, OnDestroy {
 
         // console.log(this.lastmonthclosingbalance);
         console.log('this.thismonthopenbalance', this.thismonthopenbalance)
-        if (this.thismonthopenbalance[0]["Cr/Dr"] == "CR") {
+        if (this.thismonthopenbalance && this.thismonthopenbalance.length && this.thismonthopenbalance[0]["Cr/Dr"] == "CR") {
             console.log('this.lastmonthclosingbalance["Available_Balance(INR)"] != this.thismonthopenbalance["Available_Balance(INR)"] - this.thismonthopenbalance["Transaction_Amount(INR)"]', this.lastmonthclosingbalance[0], this.thismonthopenbalance[0])
             console.log("lowerlimitstring", this.lowerlimitstring)
             if ((this.lastmonthclosingbalance && this.thismonthopenbalance) && this.lastmonthclosingbalance[0]["Available_Balance(INR)"] != this.thismonthopenbalance[0]["Available_Balance(INR)"] - this.thismonthopenbalance[0]["Transaction_Amount(INR)"]) { // *** code to not show any kind of error message in april month
